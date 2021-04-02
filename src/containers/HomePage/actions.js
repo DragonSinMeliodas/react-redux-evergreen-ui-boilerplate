47303d23a884b43d04b688f2782e709bd3ce6f52
Shubham Bansal B <shubham.b.bansal@ericsson.com>
2021-04-03 00:24:05 +0530

    integrated the store and dispath

--- src/containers/HomePage/actions.js
+++ src/containers/HomePage/actions.js
@@ -1,5 +1,5 @@
 // Function to take an argument and do an action
-
+import { ActionTypes } from "./constants";
 export const setUsers = (users) => ({
   type: ActionTypes.SET_USERS,
   payload: users,
--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { setUsers } from "./actions";
+import { makeSelectUsers } from "./selectors";
+
+//  defining which selector to use to get the state
+const stateSelector = createSelector(makeSelectUsers, (users) => ({
+  users,
+}));
+
+//  defining the action dispatch function object
+const actionDispatcher = (dispatch) => ({
+  setUser: (users) => dispatch(setUsers(users)),
+});
 
 export default function HomePage(props) {
+  const { users } = useSelector(stateSelector);
+
+  //   using hooks to call dispatch function
+  const { setUser } = actionDispatcher(useDispatch());
+
+  const fetchUsers = async () => {
+    const response = await axios
+      .get("https://reqres.in/api/users")
+      .catch((err) => {
+        console.log(`Error : ${err}`);
+      });
+
+    setUser(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  console.log("users: ", users);
+
   return <div>Hello World.!</div>;
 }
