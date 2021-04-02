f7db6fad4ec938b1701640e46ed4661fd090bf00
Shubham Bansal B <shubham.b.bansal@ericsson.com>
2021-04-02 23:48:55 +0530

    basic redux store and reducer setup

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { combineReducers, createStore } from "redux";
+import homePage from "./containers/HomePage/reducers";
+
+const reducers = combineReducers({ homePage });
+
+export default createStore(reducers);
