20f0093869f75acc37bb7eb2a23e22991f9b8522
Shubham Bansal B <shubham.b.bansal@ericsson.com>
2021-04-03 23:45:05 +0530

    Fetch Post Redux store implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import Posts from "./components/Posts";
 import "./App.css";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+
+import store from "./store";
+
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
